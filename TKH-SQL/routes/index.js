be4c8db0aa489f8d69b5668b69cf0e916b296c22
ahirsarthak <sarthakahir@gmail.com>
2022-08-22 12:45:03 -0400

    Routes Update

--- TKH-SQL/routes/index.js
+++ TKH-SQL/routes/index.js
@@ -2,58 +2,76 @@ const Router = require("express");
 const router = Router();
 const models = require("../models/index.js");
 
-router.get("/doctors", (req, res) => {
-  res.status(200).send(async () => await models.Doctors.findAll());
+router.get("/doctors", async (req, res) => {
+  try {
+    getAllDoc = await models.Doctors.findAll();
+    res.status(200).json(getAllDoc);
+  } catch (e) {
+    res.status(500).json({ error: e });
+  }
 });
-router.get("/patients", (req, res) => {
-  res.status(200).send(async () => await models.Patients.findAll());
+router.get("/patients", async (req, res) => {
+  try {
+    getAllPatient = await models.Patients.findAll();
+    res.status(200).json(getAllPatient);
+  } catch (e) {
+    res.status(500).json({ error: e });
+  }
 });
 router.post("/newDoctor", async (req, res) => {
-  let body = req.body;
-  console.log(body);
-  return await models.Doctors.create({
-    name: body.name,
-    email: body.email,
-  }).then(function (results) {
-    if (results) {
-      res.send(results);
-    } else {
-      res.status(400).send("Error in inserting new record");
-    }
-  });
+  try {
+    let body = req.body;
+    console.log(body);
+    return await models.Doctors.create({
+      name: body.name,
+      email: body.email,
+    }).then(function (results) {
+      res.status(200).json(results);
+    });
+  } catch (error) {
+    res.status(400).json("Error in inserting new record");
+  }
 });
 router.post("/newPatient", async (req, res) => {
-  let body = req.body;
-  console.log(body);
-  return await models.Patients.create({
-    name: body.name,
-    email: body.email,
-  }).then(function (results) {
-    if (results) {
-      res.send(results);
-    } else {
-      res.status(400).send("Error in inserting new record");
-    }
-  });
+  try {
+    let body = req.body;
+    console.log(body);
+    return await models.Patients.create({
+      name: body.name,
+      email: body.email,
+    }).then(function (results) {
+      res.json(results);
+    });
+  } catch (error) {
+    res.status(400).json("Error in inserting new  Patient record");
+  }
 });
-router.patch("/newDoctors", (req, res) => {
-  let body = req.body;
-  res
-    .status(200)
-    .send(
-      async () =>
-        await models.Doctors.update(
-          { name: body.newName },
-          { where: { name: body.name } }
-        )
-    );
+router.put("/newDoctors", (req, res) => {
+  try {
+    let body = req.body;
+    res
+      .status(200)
+      .json(
+        async () =>
+          await models.Doctors.update(
+            { name: body.newName },
+            { where: { name: body.name } }
+          )
+      );
+  } catch (error) {
+    res.status(400).json("Error on Put request");
+  }
 });
 router.delete("/doctors", (req, res) => {
-  let body = req.body;
-  res
-    .status(200)
-    .send(
-      async () => await models.Doctors.destroy({ where: { name: body.name } })
-    );
+  try {
+    let body = req.body;
+    res
+      .status(200)
+      .send(
+        async () => await models.Doctors.destroy({ where: { name: body.name } })
+      );
+  } catch (e) {
+    res.status(500).json({ error: e });
+  }
 });
 module.exports = router;
